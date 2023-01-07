0724c8a49affa24721cde72f15941034f4f36904
Mahmoud Elshahatt <mahmoudelshahatt1@gmail.com>
2023-01-07 10:54:12 +0200

    Add create qr-ui and genarate a QR

--- app/src/main/java/com/example/qrattendance/CreateQRFragment.kt
+++ app/src/main/java/com/example/qrattendance/CreateQRFragment.kt
@@ -1,32 +1,29 @@
 package com.example.qrattendance
 
+import android.content.Context.WINDOW_SERVICE
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Point
 import android.os.Bundle
+import android.view.*
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.example.qrattendance.databinding.FragmentCreateQRBinding
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CreateQRFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CreateQRFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentCreateQRBinding
+
+    lateinit var bitmap: Bitmap
+
+    private val nameInput by lazy {
+        binding.etAttendantName.text
     }
 
     override fun onCreateView(
@@ -34,26 +31,45 @@ class CreateQRFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_q_r, container, false)
+        binding = FragmentCreateQRBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateQRFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CreateQRFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        binding.btnCreateQr.setOnClickListener {
+
+            if (nameInput.isEmpty()) {
+                Toast.makeText(requireContext(), "Enter a name, please", Toast.LENGTH_SHORT).show()
+            } else {
+                binding.imgQR.setImageBitmap(generateQR())
+            }
+
+        }
+    }
+
+    private fun generateQR(): Bitmap {
+        val writer = QRCodeWriter()
+        try {
+            val bitMatrix =
+                writer.encode(nameInput.toString(), BarcodeFormat.QR_CODE, 600, 600)
+            val width = bitMatrix.width
+            val height = bitMatrix.height
+            bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bitmap.setPixel(x, y, if (bitMatrix[x, y]) Color.BLACK else Color.WHITE)
                 }
             }
+
+        } catch (e: WriterException) {
+            e.printStackTrace()
+        }
+        return bitmap
     }
+
 }
